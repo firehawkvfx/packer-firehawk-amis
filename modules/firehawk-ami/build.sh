980b96fc08fe3392a3c25cc72f802a50bcc46ed7
Andrew Graham <queglay@gmail.com>
2022-04-18 08:53:40 +0930

    fix args for test

--- modules/firehawk-ami/build.sh
+++ modules/firehawk-ami/build.sh
@@ -97,7 +97,7 @@ if [[ "$sourced" -eq 0 ]]; then
     packer build "$@" -var "ca_public_key_path=$HOME/.ssh/tls/ca.crt.pem" \
       -var "tls_public_key_path=$HOME/.ssh/tls/vault.crt.pem" \
       -var "tls_private_key_path=$HOME/.ssh/tls/vault.key.pem" \
-      -only "amazon-ebs.centos7-rendernode-ami"
+      -only="amazon-ebs.centos7-rendernode-ami"
       $SCRIPTDIR/firehawk-ami.pkr.hcl
 fi
 cd $EXECDIR
